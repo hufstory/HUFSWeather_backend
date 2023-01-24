de8ab15d30ab7c432054592873cae0c67cdfaab9
seungjaejeon <wjstmdwo2000@naver.com>
2023-01-24 18:55:52 +0900

    api로json추출

--- back/app.js
+++ back/app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-
+const cors = require('cors');
+var indexRouter = require('./routes/weather');
+var mainRouter = require('./routes/main');
 
 var app = express();
 
@@ -12,20 +12,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-
+app.use('/hufsWeather', mainRouter, indexRouter);
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
---
+++ back/routes/weather.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+var Axios = require('axios');
+const config = require('./api.js');
+
+/* GET home page. */
+router.get('/getWeather', async function (req, res) {
+  var url = `https://api.openweathermap.org/data/2.5/weather?lat=37.336414&lon=127.268979&lang=kr&appid=${config.api}&units=metric`
+  console.log(url)
+try {  let weatherRes = await Axios.get(
+    url 
+);
+var weather = weatherRes.data.weather[0];//weather 객체
+var temp = weatherRes.data.main.temp;//현재온도
+var pressure = weatherRes.data.main.pressure;//기압
+var humidity = weatherRes.data.main.humidity;//습도
+var temp_min = weatherRes.data.main.temp_min;//최저기온
+var temp_max = weatherRes.data.main.temp_max;//최고기온
+var windSpeed = weatherRes.data.wind.speed;//풍속
+var windDeg = weatherRes.data.wind.deg;//풍향
+var cloud = weatherRes.data.clouds.all;//구름양
+var sunrise = weatherRes.data.sys.sunrise;
+var sunset = weatherRes.data.sys.sunset;
+console.log(weather);
+console.log(temp);
+console.log(pressure);
+console.log(humidity);
+console.log(temp_min);
+console.log(temp_max);
+console.log(windSpeed);
+console.log(windDeg);
+console.log(cloud);
+
+function Unix_timestamp(t){
+  var date = new Date(t*1000);
+  var year = date.getFullYear();
+  var month = "0" + (date.getMonth()+1);
+  var day = "0" + date.getDate();
+  var hour = "0" + date.getHours();
+  var minute = "0" + date.getMinutes();
+  var second = "0" + date.getSeconds();
+  return year + "-" + month.substr(-2) + "-" + day.substr(-2) + " " + hour.substr(-2) + ":" + minute.substr(-2) + ":" + second.substr(-2);
+}
+console.log(Unix_timestamp(sunrise));//일출시간
+console.log(Unix_timestamp(sunset));//일몰시간
+return res.render('index', {
+  weather:weather,
+  temp:temp,
+  pressure:pressure,
+  humidity:humidity,
+  temp_min:temp_min,
+  temp_max:temp_max,
+  windSpeed:windSpeed,
+  windDeg:windDeg,
+  cloud:cloud,
+  sunrise:sunrise,
+  sunset:sunset})
+}
+catch(e){ 
+  res.status(500).send("error" + e);
+  return res.json(e)
+}
+});
+router.get('/designer', function(req, res){
+  res.render('designer');
+})
+module.exports = router;
