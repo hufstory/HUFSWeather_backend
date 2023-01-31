683038f494d0652f15bf6dae18bf7291d42e64ac
seungjaejeon <wjstmdwo2000@naver.com>
2023-01-31 19:31:22 +0900

    'database_connect'

--- back/app.js
+++ back/app.js
@@ -1,19 +1,41 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 const cors = require('cors');
+var mysql = require('mysql');
 var indexRouter = require('./routes/weather');
 var mainRouter = require('./routes/main');
-
+var mysqlkey = require('./routes/api');
 var app = express();
+
+var connection = mysql.createConnection({//연결할 테이블
+  host    : mysqlkey.host,
+  user    : mysqlkey.user,
+  password: mysqlkey.password,
+  database: mysqlkey.database
+});
+
+connection.connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.get('/', function(req, res){
+  var sql = 'SELECT id FROM coordinator'
+    connection.query(sql, function(err, topics, fields){
+        if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        else{
+            res.send(topics);
+        }
+    })
+})
+
+
 app.use(cors());
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
