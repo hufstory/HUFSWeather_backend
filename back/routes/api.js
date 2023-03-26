8a5ac6773bd38daa8b5161e0554f500130a0a6a5
seungjaejeon <wjstmdwo2000@naver.com>
2023-03-27 01:53:39 +0900

    2023_03_27

--- back/routes/api.js
+++
@@ -1,4 +0,0 @@
-const config = {
-  api: 'b54823b3bcf5c13a9878390184fc3006',
-}
-module.exports = config;
--- back/routes/databasekey.js
+++
@@ -1,7 +0,0 @@
-const mysqlkey = {
-  password : 'sj3165415',
-  database : 'codi',
-  user : 'root',
-  host : 'localhost'
-}
-module.exports = mysqlkey;
