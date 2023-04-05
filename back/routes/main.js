199fbd9498fa0a92a129c876340bb27bef144b67
seungjaejeon <wjstmdwo2000@naver.com>
2023-04-05 15:35:36 +0900

    JSON remaked

--- back/routes/main.js
+++ back/routes/main.js
@@ -3,11 +3,23 @@ const router = express.Router();
 const path = require('path');
 var Axios = require('axios');
 const cors = require('cors');
-const config = require('../keys/api');//api_key - openweathermap
-const config_chatgpt = require('../keys/openai_api');//api_key - chatgpt
+const config = require('../keys/api.js');//api_key - openweathermap
+const config_chatgpt = require('../keys/openai_api.js');//api_key - chatgpt
 const { Configuration, OpenAIApi } = require('openai'); //openai
 router.use(cors());
 
+
+// 타임스탬프 값을 년월일로 변환
+function Unix_timestamp(t){
+    var date = new Date(t*1000);
+    var year = date.getFullYear();
+    var month = "0" + (date.getMonth()+1);
+    var day = "0" + date.getDate();
+    var hour = "0" + date.getHours();
+    var minute = "0" + date.getMinutes();
+    var second = "0" + date.getSeconds();
+    return year + "-" + month.substr(-2) + "-" + day.substr(-2) + " " + hour.substr(-2) + ":" + minute.substr(-2) + ":" + second.substr(-2);
+}
 const configuration = new Configuration({
     organization: "org-CsLVFqxc2fo6CQj983AaTZav",
     apiKey: config_chatgpt.api,
@@ -15,91 +27,146 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 /* GET home page. */
 router.get('/', function (req, res){
-    // var url = `https://api.openweathermap.org/data/2.5/weather?lat=37.336414&lon=127.268979&lang=kr&appid=${config.api}&units=metric`
-    var url = `https://api.openweathermap.org/data/2.5/forecast?lat=37.336414&lon=127.268979&appid=${config.api}&units=metric` // 3hours 5day api url
-    Axios.get(url)
+    var message = {};
+    var url_current = `https://api.openweathermap.org/data/2.5/weather?lat=37.336414&lon=127.268979&lang=kr&appid=${config.api}&units=metric`
+    var url_3hour = `https://api.openweathermap.org/data/2.5/forecast?lat=37.336414&lon=127.268979&lang=kr&appid=${config.api}&units=metric` // 3hours 5day api url
+    Axios.get(url_current)
     .then(result => {
         const data = result.data;
-        var send_message_list = [];//json
-        var sex = 'male'
-        data.list.forEach((data) => {
-            var send_message = {
-                temp: data.main.temp, //기온
-                feel_temp: data.main.feels_like,//체감온도
-                temp_min: data.main.temp_min,//최저기온
-                temp_max: data.main.temp_max,//최고기온
-                humidity: data.main.humidity,//습도
-                weather: data.weather[0].main,//날씨
-                weather_detail: data.weather[0].description,//상세날씨
-                weather_icon: data.weather[0].icon,//그림
-                wind_speed: data.wind.speed,//풍속
-                time: data.dt_txt//시간
-            };
-            send_message_list.push(send_message);
-          });
-        openai
-        .createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages:[{role:"user", content:`
-            Weather information for the time of ${send_message_list[0].time} is as follows
-            temperatures : ${send_message_list[0].temp}
-            sensory temperature : ${send_message_list[0].feel_temp}
-            minimum temperature : ${send_message_list[0].temp_min}
-            maximum temperature${send_message_list[0].temp_max}
-            humidity : ${send_message_list[0].humidity}
-            weather : ${send_message_list[0].weather}
-            weather description : ${send_message_list[0].weather_detail}
-            wind speed : ${send_message_list[0].wind_speed}
-            Weather information for the time of ${send_message_list[1].time} is as follows
-            temperatures : ${send_message_list[1].temp}
-            sensory temperature : ${send_message_list[1].feel_temp}
-            minimum temperature : ${send_message_list[1].temp_min}
-            maximum temperature${send_message_list[1].temp_max}
-            humidity : ${send_message_list[1].humidity}
-            weather : ${send_message_list[1].weather}
-            weather description : ${send_message_list[1].weather_detail}
-            wind speed : ${send_message_list[1].wind_speed}
-            Weather information for the time of ${send_message_list[2].time} is as follows
-            temperatures : ${send_message_list[2].temp}
-            sensory temperature : ${send_message_list[2].feel_temp}
-            minimum temperature : ${send_message_list[2].temp_min}
-            maximum temperature${send_message_list[2].temp_max}
-            humidity : ${send_message_list[2].humidity}
-            weather : ${send_message_list[2].weather}
-            weather description : ${send_message_list[2].weather_detail}
-            wind speed : ${send_message_list[2].wind_speed}
-            Weather information for the time of ${send_message_list[3].time} is as follows
-            temperatures : ${send_message_list[3].temp}
-            sensory temperature : ${send_message_list[3].feel_temp}
-            minimum temperature : ${send_message_list[3].temp_min}
-            maximum temperature${send_message_list[3].temp_max}
-            humidity : ${send_message_list[3].humidity}
-            weather : ${send_message_list[3].weather}
-            weather description : ${send_message_list[3].weather_detail}
-            wind speed : ${send_message_list[3].wind_speed}
-            Weather information for the time of ${send_message_list[4].time} is as follows
-            temperatures : ${send_message_list[4].temp}
-            sensory temperature : ${send_message_list[4].feel_temp}
-            minimum temperature : ${send_message_list[4].temp_min}
-            maximum temperature${send_message_list[4].temp_max}
-            humidity : ${send_message_list[4].humidity}
-            weather : ${send_message_list[4].weather}
-            weather description : ${send_message_list[4].weather_detail}
-            wind speed : ${send_message_list[4].wind_speed}
-    
-            please answer in korean
-            `},
+        message.weather_current = {
+            temp: data.main.temp,
+            temp_max: data.main.temp_max,
+            temp_min: data.main.temp_min,
+            weather_main: data.weather[0].main,
+            weather_description: data.weather[0].description,
+            weather_icon: data.weather[0].icon
+        }
+    })
+    Axios.get(url_3hour)
+    .then(result => {
+        const data = result.data;
+        var weather_times = [];
+        var temp_min = [100,100,100,100];
+        var temp_max = [-100,-100,-100,-100];
+        // temp_min: data.list[i].main.temp_min,//최저기온
+        // temp_max: data.list[i].main.temp_max,//최고기온
+        for(var i=0; i<9; i++)
             {
-                role:"system",
-                content: "You are a system that recommends clothes to me according to the weather information I send you.\
-                Please recommend clothes suitable for this weather by synthesizing and analyzing the weather information"
+            var  weather_time_obj = {
+                temp: data.list[i].main.temp, //기온
+                feel_temp: data.list[i].main.feels_like,//체감온도
+                humidity: data.list[i].main.humidity,//습도
+                weather_main: data.list[i].weather[0].main,//날씨
+                weather_description: data.list[i].weather[0].description,//상세날씨
+                weather_icon: data.list[i].weather[0].icon,//그림
+                wind_speed: data.list[i].wind.speed,//풍속
+                time: Unix_timestamp(data.list[i].dt)//시간
+            };
+            weather_times.push(weather_time_obj);
+            };
+            message.weather_times = {
+                list : weather_times
             }
-        ]
-        })
-        .then(gpt_message=>{
-            send_message_list.push(gpt_message.data.choices[0].message.content);
-            res.json(send_message_list);
-        })
+            var weather_whole = {};
+            var weather_week ={};
+            var days = [];
+        data.list.forEach(function(list){
+            var date = Unix_timestamp(list.dt).split(' ')[0];
+            const D = new Date(date);
+            const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            const dayOfWeek = D.getDay();
+            if (!weather_whole[date]) {
+                weather_whole[date] = {
+                    minTemp: Infinity, 
+                    maxTemp: -Infinity,
+                    day : "",
+            };
+            days.push(date);
+              }
+            let minTemp = list.main.temp_min;
+            let maxTemp = list.main.temp_max;
+            if (minTemp < weather_whole[date].minTemp) {
+                weather_whole[date].minTemp = minTemp;
+            }
+            if (maxTemp > weather_whole[date].maxTemp) {
+                weather_whole[date].maxTemp = maxTemp;
+            }
+            weather_whole[date].day = weekdays[dayOfWeek];
+            // Add the forecast data to the corresponding date array
+        });
+        weather_week = {
+            firstday : weather_whole[days[0]],
+            secondday : weather_whole[days[1]],
+            thirdday : weather_whole[days[2]],
+            fourthday : weather_whole[days[3]],
+            fifthday : weather_whole[days[4]],
+        }
+        message.weather_whole = weather_week;
+        res.json(message);
+        //openai chatgpt
+        // openai
+        // .createChatCompletion({
+        //     model: "gpt-3.5-turbo",
+        //     messages:[{role:"user", content:`
+        //     Weather information for the time of ${send_message_list[0].time} is as follows
+        //     temperatures : ${send_message_list[0].temp}
+        //     sensory temperature : ${send_message_list[0].feel_temp}
+        //     minimum temperature : ${send_message_list[0].temp_min}
+        //     maximum temperature${send_message_list[0].temp_max}
+        //     humidity : ${send_message_list[0].humidity}
+        //     weather : ${send_message_list[0].weather}
+        //     weather description : ${send_message_list[0].weather_detail}
+        //     wind speed : ${send_message_list[0].wind_speed}
+        //     Weather information for the time of ${send_message_list[1].time} is as follows
+        //     temperatures : ${send_message_list[1].temp}
+        //     sensory temperature : ${send_message_list[1].feel_temp}
+        //     minimum temperature : ${send_message_list[1].temp_min}
+        //     maximum temperature${send_message_list[1].temp_max}
+        //     humidity : ${send_message_list[1].humidity}
+        //     weather : ${send_message_list[1].weather}
+        //     weather description : ${send_message_list[1].weather_detail}
+        //     wind speed : ${send_message_list[1].wind_speed}
+        //     Weather information for the time of ${send_message_list[2].time} is as follows
+        //     temperatures : ${send_message_list[2].temp}
+        //     sensory temperature : ${send_message_list[2].feel_temp}
+        //     minimum temperature : ${send_message_list[2].temp_min}
+        //     maximum temperature${send_message_list[2].temp_max}
+        //     humidity : ${send_message_list[2].humidity}
+        //     weather : ${send_message_list[2].weather}
+        //     weather description : ${send_message_list[2].weather_detail}
+        //     wind speed : ${send_message_list[2].wind_speed}
+        //     Weather information for the time of ${send_message_list[3].time} is as follows
+        //     temperatures : ${send_message_list[3].temp}
+        //     sensory temperature : ${send_message_list[3].feel_temp}
+        //     minimum temperature : ${send_message_list[3].temp_min}
+        //     maximum temperature${send_message_list[3].temp_max}
+        //     humidity : ${send_message_list[3].humidity}
+        //     weather : ${send_message_list[3].weather}
+        //     weather description : ${send_message_list[3].weather_detail}
+        //     wind speed : ${send_message_list[3].wind_speed}
+        //     Weather information for the time of ${send_message_list[4].time} is as follows
+        //     temperatures : ${send_message_list[4].temp}
+        //     sensory temperature : ${send_message_list[4].feel_temp}
+        //     minimum temperature : ${send_message_list[4].temp_min}
+        //     maximum temperature${send_message_list[4].temp_max}
+        //     humidity : ${send_message_list[4].humidity}
+        //     weather : ${send_message_list[4].weather}
+        //     weather description : ${send_message_list[4].weather_detail}
+        //     wind speed : ${send_message_list[4].wind_speed}
+    
+        //     please answer in korean
+        //     `},
+        //     {
+        //         role:"system",
+        //         content: "You are a system that recommends clothes to me according to the weather information I send you.\
+        //         Please recommend clothes suitable for this weather by synthesizing and analyzing the weather information"
+        //     }
+        // ]
+        // })
+        // .then(gpt_message=>{
+        //     send_message_list.push(gpt_message.data.choices[0].message.content);
+        //     res.json(send_message_list);
+        // })
 
     })
     .catch(e=>{
